51040d6f2360839efc20f9fbc15119676f3c4b5e
ABHINAV KARFORMA <abhinavkarforma2@gmail.com>
2021-12-28 18:13:02 +0530

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -2,7 +2,7 @@ module.exports={
     name:'unban',
     description:'This is used to unban a member',
     execute(message,args){
-        message.guild.members.unban("732300796816785457");
+        message.guild.members.unban("");
         return message.reply(`It has been unbanned`);
     }
 }
